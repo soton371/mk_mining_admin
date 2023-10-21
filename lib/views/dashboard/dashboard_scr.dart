4acca8cd833a0bef71e3cdfc5d2c4abf43db5ba7
Soton <soton371@gmail.com>
2023-10-21 21:04:53 +0600

    icon

--- lib/views/dashboard/dashboard_scr.dart
+++ lib/views/dashboard/dashboard_scr.dart
@@ -10,35 +10,46 @@ class DashboardScreen extends StatelessWidget {
         title: const Text("Users"),
       ),
       body: ListView.builder(
-        physics: const BouncingScrollPhysics(),
+          physics: const BouncingScrollPhysics(),
           itemCount: 10,
           padding: const EdgeInsets.symmetric(horizontal: 8),
           itemBuilder: (_, i) => Card(
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
-
                   children: [
-                    
                     const Padding(
                       padding: EdgeInsets.all(8.0),
                       child: CircleAvatar(
-                        child: Text("S",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                        child: Text(
+                          "S",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
-                     Expanded(
+                    Expanded(
                       child: Padding(
                         padding: const EdgeInsets.symmetric(vertical: 8),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            const Text("Soton Ahmed",style: TextStyle(fontWeight: FontWeight.bold),),
+                            const Text(
+                              "Soton Ahmed",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
                             const Text("tasmia437@gmail.com"),
-                            Text("Main: 500 | Mining: 01 | Team: 08",style: TextStyle(fontSize: 12,color: Theme.of(context).colorScheme.onSurfaceVariant),),
+                            Text(
+                              "Main: 500 | Mining: 01 | Team: 08",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurfaceVariant),
+                            ),
                           ],
                         ),
                       ),
                     ),
-                    
                     Switch(value: false, onChanged: (v) {}),
                   ],
                 ),
